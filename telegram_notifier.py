06d00fc864b8baab6273183fa1a95c5d1bbdff13
vamsi <vamsikrishnagannamaneni@gmail.com>
2022-11-20 16:12:38 +0530

    'changes'

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -35,11 +35,11 @@ timeframes_dict={
 
 
 coin='ETH'
-timeframe_1='5m'  #dont forget to change sleep time accordingly
-period_1=7
-atr1_1=1
-pivot_period_1=1
-ma_condition_1='ema_100'
+timeframe_1='15m'  #dont forget to change sleep time accordingly
+period_1=56
+atr1_1=3
+pivot_period_1=3
+ma_condition_1='ema_200'
 time_1=timeframes_dict[timeframe_1]
 
 client=Client(config.api_key,config.secret_key)
@@ -47,11 +47,11 @@ client=Client(config.api_key,config.secret_key)
 client.futures_change_leverage(symbol=f'{coin}USDT', leverage=1)
 client.futures_change_leverage(symbol=f'{coin}BUSD', leverage=1)
 
-timeframe_busd='15m'  #dont forget to change sleep time accordingly
-period_busd=7
-atr1_busd=1
-pivot_period_busd=1
-ma_condition_busd='ema_100'
+timeframe_busd='1d'  #dont forget to change sleep time accordingly
+period_busd=56
+atr1_busd=3
+pivot_period_busd=3
+ma_condition_busd='ema_200'
 time_2=timeframes_dict[timeframe_busd]
 
 
