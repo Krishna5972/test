b22f299303e7a5bc39f33ebfc50e58078fd17769
vamsi <vamsikrishnagannamaneni@gmail.com>
2022-12-22 01:35:05 +0530

    balance

--- bot_funtions.py
+++ bot_funtions.py
@@ -381,12 +381,14 @@ def condition_busdt(timeframe,pivot_period,atr1,period,ma_condition,exchange,cli
                     temp_df['OpenTime']=[datetime.fromtimestamp(x/1000) for x in temp_df['OpenTime']]
                     df=pd.concat([df,temp_df])
                     df=df[2:]
-                    
-                    super_df=supertrend(coin,df, period, atr1,pivot_period)
-                    super_df[f'{ma_condition}_pos']=super_df[[ma_condition,'close']].apply(ema_pos,col_name=ma_condition,axis=1)
-                    ma_pos=super_df.iloc[-1][f'{ma_condition}_pos']
-                    print('here')
                     print(df)
+                    super_df=supertrend(coin,df, period, atr1,pivot_period)
+                    print('here_1')
+                    super_df[f'{ma_condition}_pos']=super_df[[ma_condition,'close']].apply(ema_pos,col_name=ma_condition,axis=1)
+                    print('here_2')
+                    ma_pos=super_df.iloc[-1][f'{ma_condition}_pos']
+                    print('here_3')
+                    
                     print(super_df)
                     if super_df.iloc[-1]['in_uptrend'] != super_df.iloc[-2]['in_uptrend']:
                         lock.acquire()
